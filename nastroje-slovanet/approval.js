e1261902cdb996a5396df781b1e0a0769911dc2c
Samott2 <63498283+Samott2@users.noreply.github.com>
2022-12-19 13:47:15 +0100

    Update approval.js

--- nastroje-slovanet/approval.js
+++ nastroje-slovanet/approval.js
@@ -1,3 +1,14 @@
+//Zapracoval Tomáš P. 19.12.2022
+(() => {
+  let canAccessUdoMetaPromise = null;
+  const onNavigate = async () => {
+    const canAccessUdoMetaResult = await (canAccessUdoMetaPromise || (canAccessUdoMetaPromise = common.canAccessUdoMeta()));
+    if (!canAccessUdoMetaResult) {
+      return ui.showAccessRejectedDialog();
+    }
+  };
+//Zapracoval Tomáš P. 19.12.2022
+  
 const approval = (() => {
   const APPROVAL_UDO_META_NAME = 'Schvalovanie';
   const APPROVAL_STATUS = {
